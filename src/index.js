1b2dae62a6239a931d82b394a6b4a275947a62b5
Cristian Guerrero <guerreroc158@gmail.com>
2025-04-09 20:02:40 -0600

    correccion a web socket

--- src/index.js
+++ src/index.js
@@ -8,13 +8,10 @@ const consign = require('consign');
 const cookieParser = require('cookie-parser');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-  // cors: {
-  //   origin: "https://suite-mantenimiento.tlea.online",
-  //   methods: ["GET", "POST"],
-  //   credentials: true
-  // }
-  cors:{
-    origin: "http://localhost:3009",
+  cors: {
+    origin: "https://suite-mantenimiento.tlea.online",
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
@@ -25,11 +22,10 @@ app.use(express.urlencoded({extended: true, limit: '500mb', parameter: 500000}))
 app.use(express.json({limit: '500mb'}));
 app.use('/evidencias', express.static('evidencias'));
 app.use(cookieParser());
-app.use(cors());
-// app.use(cors({
-//   origin: "https://suite-mantenimiento.tlea.online",
-//   credentials: true
-// }));
+app.use(cors({
+  origin: "https://suite-mantenimiento.tlea.online", // sin slash
+  credentials: true
+}));
 
 consign({cwd: 'src'})
 .include('libs/config.js')
