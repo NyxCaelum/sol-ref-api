2da8dc1aea82eb135bd16f95302ba7efe334c39a
Cristian Guerrero <guerreroc158@gmail.com>
2025-12-10 15:20:51 -0600

    database cambios

--- src/database.js
+++ src/database.js
@@ -4,12 +4,30 @@ const fs = require('fs');
 
 let db = null;
 
+function loadModelsRecursively(sequelize, Sequelize, dir, models) {
+    fs.readdirSync(dir).forEach(file => {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            // Si es carpeta, la recorremos recursivamente
+            loadModelsRecursively(sequelize, Sequelize, fullPath, models);
+        } else if (file.endsWith('.js')) {
+            // Solo cargamos archivos JS
+            const model = require(fullPath)(sequelize, Sequelize.DataTypes);
+            models[model.name] = model;
+        }
+    });
+}
+
 module.exports = app => {
-    
-    if (!db) {        
+    if (!db) {
         const config = app.libs.config;
 
-        const sequelize = new Sequelize(config.database, config.username, config.password,
+        const sequelize = new Sequelize(
+            config.database,
+            config.username,
+            config.password,
             config.options
         );
 
@@ -19,20 +37,15 @@ module.exports = app => {
             models: {}
         };
 
-        const dir = path.join(__dirname, 'models');
-        fs.readdirSync(dir).forEach(filename => {
-            const modelDir = path.join(dir, filename);
-            const model = sequelize.import(modelDir);
-            db.models[model.name] = model;
-        });
+        const modelsDir = path.join(__dirname, 'models');
+        loadModelsRecursively(sequelize, Sequelize, modelsDir, db.models);
 
-        
-        Object.keys(db.models).forEach(key => {            
-            if (db.models[key].associate) {                
+        // Asociaciones
+        Object.keys(db.models).forEach(key => {
+            if (db.models[key].associate) {
                 db.models[key].associate(db.models);
             }
         });
-
     }
 
     return db;
