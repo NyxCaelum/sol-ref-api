a69af92b932ffda2b892a46ba828fbcaa9608174
Cristian Guerrero <guerreroc158@gmail.com>
2025-12-10 14:42:45 -0600

    camibo db

--- src/libs/config.js
+++ src/libs/config.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv').config();
 module.exports = {
     database: process.env.DATABASE || 'solicitud_refacciones',
     username: process.env.USER_DATABASE || 'devhjc',
-    password: process.env.PASSWORD_DATABASE || 'TLEA_2023_suite_hr!',   
+    password: process.env.PASSWORD_DATABASE || 'TLEA_2025_db_25k!!!',   
     options:{
         logging: false,
         host: process.env.HOST_DATABASE ||'localhost',
